f2817c6c3c10fa6b7616b6e3a4273672027200b6
vlmatv13 <vladislavmatvejev13@gmail.com>
2016-11-28 10:12:01 +0200

    Update middleware.php

--- src/middleware.php
+++ src/middleware.php
@@ -2,10 +2,3 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
-/*$app->add(function ($request, $response, $next) {
-    $response->getBody()->write('BEFORE');
-    $response = $next($request, $response);
-    $response->getBody()->write('AFTER');
-
-    return $response;
-});*/
