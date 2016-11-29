0f7558b90b83e4cda59f22d51e7130fe5b9b80b8
vlmatv13 <vladislavmatvejev13@gmail.com>
2016-11-29 18:57:27 +0200

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,20 +1,7 @@
 <?php
 // Routes
 
-/*$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
-});*/
-
-
-/*$app->get('/', function ($request, $response, $args) {
-    $response->getBody()->write(' Hello ');
-
-    return $response;
-});*/
 
 
 $app->get('/api/{type}/{value}', '\Controller:getCustomer');
